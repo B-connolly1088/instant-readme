37bddd77b12de7ffb3e143f76c901343c42e03ba
Ben laptop <b.connolly1088@gmail.com>
2023-06-09 09:01:39 -0600

    added html template and inquirer prompts

---
+++ index.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Please type a detailed description of the application or project.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Please type your Table of Contents here.',
+            name: 'contents',
+        },
+        {
+            type: 'input',
+            message: 'Please describe installation process.',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Please explain how to use the application.',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Please choose a license.',
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'Who else contributed to this project?',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'Explain any tests associated with this application.',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'Please enter information to where people can reach you with questions about the application.',
+            name: 'questions',
+        },
+    ]).then (function(response) {
+        console.log(response)
+        const { description, contents, installation, usage, license, contribution, test, questions } = response;
+        const template = `
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <h3>## Description</h3>
+        <p></p>
+    </section>
+    <section>
+        <h3>## Table of Contents</h3>
+        <p></p>
+    </section>
+    <section>
+        <h3>## Installation</h3>
+        <p></p>
+    </section>
+    <section>
+        <h3>## Usage</h3>
+        <p></p>
+    </section>
+    <section>
+        <h3>## License</h3>
+        <p></p>
+    </section>
+    <section>
+        <h3>## Contributing</h3>
+        <p></p>
+    </section>
+    <section>
+        <h3>## Tests</h3>
+        <p></p>
+    </section>
+    <section>
+        <h3>## Questions</h3>
+        <p></p>
+    </section>
+    <script src="./index.js"></script>
+</body>
+</html>
+        `
+    })
